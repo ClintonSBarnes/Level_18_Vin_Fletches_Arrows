bda396242d669ca2d287772d5e19ef6a0c4a9a8d
ClintonSBarnes <clintbarnes8@gmail.com>
2022-05-28 15:24:21 -0500

    Add project files.

---
+++ Arrows.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level_18_Vin_Fletches_Arrows
+{
+    public class Arrows
+    {
+        //establishes variables
+        public string _arrowhead = "1";
+        public float _shaft = 0.00f;
+        public string _fletching = "2";
+        private string userInput = "3";
+        public float _arrowCost = 0.00f;
+
+        //allows user to select the arrowhead material, adds to the cost of the arrow, and validates user input
+        public float SelectArrowHead()
+        {
+            Console.WriteLine(
+
+            $"\n--------------------------------------------------------------------------------------" +
+            $"\nWe have several arrow heads you can select from. Please choose one:\n " +
+            $"\n{ArrowHeads.Wood} for {(int)ArrowHeads.Wood} gold\n{ArrowHeads.Steel} for {(int)ArrowHeads.Steel} gold\n" +
+            $"{ArrowHeads.Obsidian} for {(int)ArrowHeads.Obsidian} gold " +
+            $"\n--------------------------------------------------------------------------------------");
+            userInput = Console.ReadLine();
+
+            while (_arrowhead != userInput)
+            {
+                if (userInput == "wood" || userInput == "Wood")
+                {
+                    _arrowhead = userInput;
+                    _arrowCost += 3;
+                    break;
+
+                }
+                else if (userInput == "steel" || userInput == "Steel")
+                {
+                    _arrowhead = userInput;
+                    _arrowCost += 10;
+                    break;
+                }
+                else if (userInput == "obsidian" || userInput == "Obsidian")
+                {
+                    _arrowhead = userInput;
+                    _arrowCost += 5;
+                    break;
+                }
+                else
+                    Console.WriteLine("You have made an invalid choice. Please select again.");
+                userInput = Console.ReadLine();
+
+            }
+
+            return _arrowCost;
+
+        }
+
+        //allows user to select the Flethching material, adds to the cost of the arrow, and validates user input
+
+        public float SelectArrowFletching()
+        {
+            Console.WriteLine($"\n--------------------------------------------------------------------------------------\n" +
+                $"We have several arrow heads you can select from. Please choose one:" +
+            $"\n{Fletching.Plastic} for {(int)Fletching.Plastic} gold, turkey feathers for {(int)Fletching.turkeyFeathers} gold, " +
+            $"or goose feathers for {(int)Fletching.gooseFeathers} gold" +
+            $"\n--------------------------------------------------------------------------------------");
+            userInput = Console.ReadLine();
+
+            while (_fletching != userInput)
+            {
+                if (userInput == "plastic" || userInput == "Plastic")
+                {
+                    _fletching = userInput;
+                    _arrowCost += 10;
+                    break;
+
+                }
+                else if (userInput == "Turkey Feathers" || userInput == "turkey feathers" || userInput == "Turkey feathers" || userInput == "turkey Feathers")
+                {
+                    _fletching = userInput;
+                    _arrowCost += 5;
+                    break;
+                }
+                else if (userInput == "Goose Feathers" || userInput == "goose feathers" || userInput == "Goose feathers" || userInput == "goose Feathers")
+                {
+                    _fletching = userInput;
+                    _arrowCost += 3;
+                    break;
+                }
+                else
+                    Console.WriteLine("You have made an invalid choice. Please select again.");
+                userInput = Console.ReadLine();
+
+            }
+            return _arrowCost;
+        }
+
+        //allows user to select the length of the arrow and adds to the cost of the arrow
+
+        public float GetShaftLength()
+        {
+            Console.WriteLine("\n--------------------------------------------------------------------------------------" +
+                "\nHow long would you like the shaft of the arrow to be? The cost is 0.05 gold per centimeter." +
+                "\n--------------------------------------------------------------------------------------");
+            _shaft = Convert.ToSingle(Console.ReadLine());
+            _arrowCost += _shaft * .05f;
+            return _arrowCost;
+
+
+        }
+    }
+}
+
+enum ArrowHeads
+{
+    Steel = 10,
+    Wood = 3,
+    Obsidian = 5
+}
+
+enum Fletching
+{
+    Plastic = 10,
+    turkeyFeathers = 5,
+    gooseFeathers = 3
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Level_18_Vin_Fletches_Arrows.Arrows[] purchase = new Level_18_Vin_Fletches_Arrows.Arrows[25];
+//Level_18_Vin_Fletches_Arrows.Arrows purchase = new Level_18_Vin_Fletches_Arrows.Arrows();
+string quiverFill = "Y";
+int arrowCount = 1;
+float totalCost = 0f;
+
+
+//VERY IMPORTANT: This initializes each member of the array.
+for (int i = 0; i < purchase.Length; i++)
+{
+    purchase[i] = new Level_18_Vin_Fletches_Arrows.Arrows();
+}
+
+
+//iterates the user defined arrows for as long as the user wants until they have purchased 25 arrows.
+for (int i = 0; i < 26; i++)
+{
+   
+purchase[i].SelectArrowHead();
+purchase[i].SelectArrowFletching();
+purchase[i].GetShaftLength();
+
+Console.WriteLine($"\n--------------------------------------------------------------------------------------" +
+    $"\nThat is a fine arrow with a {purchase[i]._shaft}cm shaft, a flething of {purchase[i]._fletching}, and a head of {purchase[i]._arrowhead}! " +
+    $"It will only cost you {purchase[i]._arrowCost} gold");
+
+    //adds cost of the current arrow to the total of all arrows.
+    totalCost = purchase[i]._arrowCost;
+
+    //offers the chance to continue purchasing arrows as long as the 25-quiver slots have not been filled.
+    Console.WriteLine($"You have purchased {arrowCount}. You can purchase enough (25) to fill your quiver if you would like.");
+    Console.WriteLine("\n--------------------------------------------------------------------------------------" +
+        "\nWould you like to continue filling your quiver? (Y/N)" +
+        "\n--------------------------------------------------------------------------------------");
+
+    //checks the count of arrows in the quiver
+    quiverFill = Console.ReadLine();
+    if (quiverFill == "N" || arrowCount==25 || quiverFill == "n")
+    {
+        break;
+    }
+    Console.WriteLine("You have purchased all the arrows your quiver will hold! We are so lucky to have you as a customer!" +
+        "\n--------------------------------------------------------------------------------------");
+}
+
+Console.WriteLine($"Let's see...that is {arrowCount} arrow(s), and the total comes to {totalCost}. Thank you for your buisiness, kind sir!");
+
+
+
+
+
+Console.ReadKey();
+
+
+
